c1a8e8f9fb2ce2d141e67f603de3d60eb031c62b
Andrea Tacchetti <atacchet@mit.edu>
2012-01-25 15:02:56 -0500

    Added a few comments to the demo scripts

--- gurls/demo/gurlsdemo.m
+++ gurls/demo/gurlsdemo.m
@@ -1,6 +1,12 @@
 load('./data/yeast_data.mat');
 
+% This executes 5 times four different pipelines on the same dataset.
+% it then uses the summary routines to visualize precision and accuracy
+% for each class and globally for each pipeline.
+
 for i = 1:5
+
+	% Linear kernel, primal formulation, Leave One Out cross validation to select lambda
 	name = ['linloo_' num2str(i)];
 	opt = defopt(name);
 	opt.seq = {'paramsel:loocvprimal','rls:primal','pred:primal','perf:precrec','perf:macroavg'};
@@ -9,6 +15,7 @@ for i = 1:5
 	gurls (Xtr, ytr, opt,1)
 	gurls (Xte, yte, opt,2)
 	
+	% Gaussian kernel, (dual formulation), Leave One Out cross validation to select lambda and the Kernel width sigma	
 	name = ['rbfloo_' num2str(i)];
 	opt = defopt(name);
 	opt.seq = {'paramsel:siglam', 'kernel:rbf', 'rls:dual', 'predkernel:traintest', 'pred:dual', 'perf:macroavg', 'perf:precrec'};
@@ -18,6 +25,7 @@ for i = 1:5
 	gurls (Xte, yte, opt,2)
 	
 	
+	% Linear kernel, primal formulation, Hold Out cross validation to select lambda
 	name = ['linho_' num2str(i)];
 	opt = defopt(name);
 	opt.seq = {'split:ho','paramsel:hoprimal','rls:primal','pred:primal','perf:macroavg','perf:precrec'};
@@ -27,6 +35,7 @@ for i = 1:5
 	gurls (Xte, yte, opt,2)
 	
 	
+	% Gaussian kernel, (dual formulation), Hold Out cross validation to select lambda and the Kernel width sigma	
 	name = ['rbfho_' num2str(i)];
 	opt = defopt(name);
 	opt.seq = {'split:ho', 'paramsel:siglamho', 'kernel:rbf', 'rls:dual', 'predkernel:traintest', 'pred:dual', 'perf:macroavg', 'perf:precrec'};
@@ -37,12 +46,23 @@ for i = 1:5
 
 end
 
+% Visualization: 
+%	- filestr contains the names of the experiments
+%	- nRuns contains the number of runs for each experiment
+%	- fields contains which fields of opt we want to display
+%		(as many plots as the elements of fields will be generated).
+%	- plotopt is a structure containing various niceties for the plots.
+
 filestr = {'linloo','rbfloo','linho','rbfho'};
 nRuns = {5,5,5,5};
 fields = {'perf.ap','perf.acc'};
 plotopt.titles = {'Model Comparison - Accuracy','Model Comparison - Precision'};
 plotopt.class_names = {'CYT','NUC','MIT','ME3'};
+
+% This generates "per-class" plots
 summary_plot(filestr,fields,nRuns,plotopt)  
+% This generates "global" plots
 summary_overall_plot(filestr,fields,nRuns,plotopt)  
+% This plots times taken by each step of the pipeline for performance reference.
 plot_times(filestr,nRuns)
 
