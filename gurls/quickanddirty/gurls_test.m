97319646549da8907ab6ab6cf159dedecc817124
Andrea Tacchetti <atacchet@mit.edu>
2012-05-17 17:40:11 -0400

    "quick and dirty" methods documented

--- gurls/quickanddirty/gurls_test.m
+++ gurls/quickanddirty/gurls_test.m
@@ -1,5 +1,24 @@
 function [yhat, acc] = gurls_test(X,y,opt)
 
+%	gurls_test(X,y,opt)
+%	Predicts labels on a test-set using a linear model.
+%	The regularization parameter is chosen using a leave-one-out crossvalidation.
+%	
+%	INPUTS:
+%		- X : 	input data matrix (one sample per row)
+%		- y : 	labels (must have as many entries as there are rows in X).
+%			must contain integers [1...T] with T being the number of classes.
+%		- opt : Structure containing the fields outlined in gurls_train.m
+%
+%	OUTPUTS:
+%		- yhat :	predicted labels (will have as many entries as there are rows in X.)
+%		- acc  : 	Accuracy per class ( i.e. tp/(tp+fn)  per class ).
+%		- The following fields are appended to opt:
+%			* pred	: output of pred_dual.m
+%			* perf	: output of perf_macroavg.m
+%
+%	The opt structure will be save to disk in a file called "quickanddirty.mat"
+
 	T = max(y);
 	codes = 2*eye(T) - 1;
 	y = codes(y,:);
