6780c844e605253348a61640e2d490fa05d05ca4
Matteo Santoro <matteo.santoro@gmail.com>
2012-12-21 13:09:44 +0100

    removed old demo

--- bgurls/demo/code/bgTrainRun.m
+++
@@ -1,5 +0,0 @@
-function [] = bgTrainRun(wpath)
-		admMatMultRun(fullfile(wpath,'XtX.mat'));
-		admMatMultRun(fullfile(wpath,'Xty.mat')); 
-		admMatMultRun(fullfile(wpath,'XvatXva.mat'));
-		admMatMultRun(fullfile(wpath,'Xvatyva.mat'));
