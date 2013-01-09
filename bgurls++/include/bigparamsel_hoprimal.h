102d7f42bf004ca43f6dd81a7a229e24f6a8362e
Matteo Santoro <matteo.santoro@gmail.com>
2013-01-09 13:16:10 +0100

    Implemented bigparamsel_hoprimal

--- bgurls++/include/bigparamsel_hoprimal.h
+++ bgurls++/include/bigparamsel_hoprimal.h
@@ -45,6 +45,10 @@
 
 
 #include "bigparamsel.h"
+#include "optmatrix.h"
+#include "bigmath.h"
+#include "bigpred_primal.h"
+#include "bigperf.h"
 
 
 namespace gurls
@@ -82,11 +86,166 @@ public:
 };
 
 template <typename T>
-GurlsOptionsList *BigParamSelHoPrimal<T>::execute(const BigArray<T> &/*X*/, const BigArray<T> &/*Y*/, const GurlsOptionsList &/*opt*/)
+GurlsOptionsList *BigParamSelHoPrimal<T>::execute(const BigArray<T> &X, const BigArray<T> &Y, const GurlsOptionsList &opt)
 {
-    // TODO
 
-    return new GurlsOptionsList("paramsel");
+    const GurlsOptionsList* split = opt.getOptAs<GurlsOptionsList>("split");
+    const BigArray<T>& Xva = split->getOptValue<OptMatrix<BigArray<T> > >("Xva");
+    const BigArray<T>& Yva = split->getOptValue<OptMatrix<BigArray<T> > >("Yva");
+
+    BigArray<T>* XtX = matMult_AtB(X, X, opt.getOptAsString("files.XtX_filename"), opt.getOptAsNumber("memlimit"));
+    BigArray<T>* Xty = matMult_AtB(X, Y, opt.getOptAsString("files.Xty_fileName"), opt.getOptAsNumber("memlimit"));
+    BigArray<T>* XvatXva = matMult_AtB(Xva, Xva, opt.getOptAsString("files.XvatXva_filename"), opt.getOptAsNumber("memlimit"));
+    BigArray<T>* Xvatyva = matMult_AtB(Xva, Yva, opt.getOptAsString("files.Xvatyva_fileName"), opt.getOptAsNumber("memlimit"));
+
+
+//    K = XtX - XvatXva;
+    gMat2D<T>* XtX_mat = new gMat2D<T>(XtX->rows(), XtX->cols());
+    XtX->getMatrix(0,0, *XtX_mat);
+    delete XtX;
+
+    gMat2D<T>* XvatXva_mat = new gMat2D<T>(XvatXva->rows(), XvatXva->cols());
+    XvatXva->getMatrix(0,0, *XvatXva_mat);
+    delete XvatXva;
+
+    axpy(XtX_mat->getSize(), (T)-1.0, XvatXva_mat->getData(), 1, XtX_mat->getData(), 1);
+    delete XvatXva_mat;
+
+//  Xty = Xty - Xvatyva;
+    gMat2D<T>* Xty_mat = new gMat2D<T>(Xty->rows(), Xty->cols());
+    Xty->getMatrix(0,0, *Xty_mat);
+    delete Xty;
+
+    gMat2D<T>* Xvatyva_mat = new gMat2D<T>(Xvatyva->rows(), Xvatyva->cols());
+    Xvatyva->getMatrix(0,0, *Xvatyva_mat);
+    delete Xvatyva;
+
+    axpy(Xty_mat->getSize(), (T)-1.0, Xvatyva_mat->getData(), 1, Xty_mat->getData(), 1);
+    delete Xvatyva_mat;
+
+
+    const unsigned long n = X.rows() - Xva.rows();
+    const unsigned long d = X.cols();
+    const unsigned long t = Y.cols();
+
+
+    int tot = static_cast<int>(std::ceil( opt.getOptAsNumber("nlambda")));
+
+
+//	[Q,L] = eig(K);
+    T* Q = XtX_mat->getData();
+    T *L = new T[d];
+
+    eig_sm(Q, L, d);
+
+
+//	QtXtY = Q'*Xty;
+    T* QtXtY = new T[d*t];
+    dot(Q, Xty_mat->getData(), QtXtY, d, d, d, t, d, t, CblasTrans, CblasNoTrans, CblasColMajor);
+    delete Xty_mat;
+
+//	guesses = paramsel_lambdaguesses(L, min(n,d), n, opt);
+    T* guesses = lambdaguesses(L, d, std::min(d, n), n, tot, (T)(opt.getOptAsNumber("smallnumber")));
+
+//	ap = zeros(tot,T);
+    gMat2D<T>* ap_mat = new gMat2D<T>(tot, t);
+    T* ap = ap_mat->getData();
+    set(ap, (T)0.0, tot*t);
+
+
+    GurlsOptionsList* nestedOpt = new GurlsOptionsList("nested");
+    nestedOpt->copyOpt("nb_pred", opt);
+    nestedOpt->copyOpt("files", opt);
+    nestedOpt->copyOpt("tmpfile", opt);
+    nestedOpt->copyOpt("memlimit", opt);
+
+    GurlsOptionsList* optimizer = new GurlsOptionsList("optimizer");
+    nestedOpt->addOpt("optimizer",optimizer);
+
+    T* work = new T[d*(d+1)];
+
+    gMat2D<T> *W = new gMat2D<T>(d, t);
+    optimizer->addOpt("W", new OptMatrix<gMat2D<T> >(*W));
+
+    BigPredPrimal< T > primal;
+    BigPerformance<T>* perfClass = BigPerformance<T>::factory(opt.getOptAsString("hoperf"));
+
+
+    for(int i=0; i<tot; ++i)
+    {
+//        opt.rls.W = rls_eigen(Q,L,QtXtY,guesses(i),n);
+        rls_eigen(Q, L, QtXtY, W->getData(), guesses[i], n, d, d, d, d, t, work);
+
+//        opt.pred = bigpred_primal(Xva,yva,opt);
+        GurlsOptionsList *ret_pred = primal.execute(Xva, Yva, *nestedOpt);
+
+        nestedOpt->removeOpt("pred");
+        nestedOpt->addOpt("pred", ret_pred);
+
+//		opt.perf = opt.hoperf(Xva,yva,opt);
+        GurlsOptionsList* ret_perf = perfClass->execute(Xva, Yva, *nestedOpt);
+
+        gMat2D<T> &forho_vec = ret_perf->getOptValue<OptMatrix<gMat2D<T> > >("forho");
+
+        copy(ap+i, forho_vec.getData(), t, tot, 1);
+
+        delete ret_perf;
+    }
+
+    delete [] work;
+    delete nestedOpt;
+
+    delete perfClass;
+    delete [] XtX_mat;
+    delete [] L;
+    delete [] QtXtY;
+
+
+    GurlsOptionsList* paramsel;
+
+    if(opt.hasOpt("paramsel"))
+    {
+        GurlsOptionsList* tmp_opt = new GurlsOptionsList("tmp");
+        tmp_opt->copyOpt("paramsel", opt);
+
+        paramsel = GurlsOptionsList::dynacast(tmp_opt->getOpt("paramsel"));
+        tmp_opt->removeOpt("paramsel", false);
+        delete tmp_opt;
+
+        paramsel->removeOpt("lambdas");
+        paramsel->removeOpt("forho");
+        paramsel->removeOpt("guesses");
+    }
+    else
+        paramsel = new GurlsOptionsList("paramsel");
+
+
+
+//    [dummy,idx] = max(ap,[],1);
+    work = NULL;
+    unsigned long* idx = new unsigned long[t];
+    indicesOfMax(ap, tot, t, idx, work, 1);
+
+//	vout.lambdas = guesses(idx);
+    gMat2D<T>* lambdas = new gMat2D<T>(1, t);
+    copyLocations(idx, guesses, t, tot, lambdas->getData());
+    delete [] idx;
+
+    paramsel->addOpt("lambdas", new OptMatrix<gMat2D<T> >(*lambdas));
+
+
+//	vout.forho = ap;
+    paramsel->addOpt("forho", new OptMatrix<gMat2D<T> >(*ap_mat));
+
+//	vout.guesses = guesses;
+    gMat2D<T>* guesses_mat = new gMat2D<T>(1, tot);
+    copy(guesses_mat->getData(), guesses, tot);
+    delete[] guesses;
+
+    paramsel->addOpt("guesses", new OptMatrix<gMat2D<T> >(*guesses_mat));
+
+
+    return paramsel;
 }
 
 }
