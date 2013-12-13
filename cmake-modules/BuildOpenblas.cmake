61f94ef4ad66e8be3b2669233543502abd912bb6
Elena Ceseracciu <eowyn83@gmail.com>
2013-12-13 16:53:17 +0100

    Download Openblas version 0.2.8; fixes in the build/find mechanism for Openblas

--- cmake-modules/BuildOpenblas.cmake
+++ cmake-modules/BuildOpenblas.cmake
@@ -17,18 +17,25 @@ if (MSVC)
 else()
     ExternalProject_add(buildOpenblas
         GIT_REPOSITORY "git://github.com/xianyi/OpenBLAS.git"
-        GIT_TAG "v0.2.6"
+        GIT_TAG "v0.2.8"
         UPDATE_COMMAND ""
         BUILD_IN_SOURCE 1
         SOURCE_DIR  ${EXTERNAL_PREFIX}/src/openblas
-        INSTALL_DIR ${EXTERNAL_PREFIX}/
+        INSTALL_DIR ${EXTERNAL_PREFIX}
         CONFIGURE_COMMAND ""
-        PATCH_COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/Makefile.install.openblas" "<SOURCE_DIR>/Makefile.install"
+        PATCH_COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_LIST_DIR}/../external/Makefile.install.openblas" "<SOURCE_DIR>/Makefile.install"
         BUILD_COMMAND NO_SHARED=1 ${CMAKE_MAKE_PROGRAM} libs netlib
         INSTALL_COMMAND NO_SHARED=1 ${CMAKE_MAKE_PROGRAM} install PREFIX=<INSTALL_DIR>
     )
 
     set(Openblas_FOUND TRUE)
+    #set(Openblas_LIBRARIES ${Openblas_LIBRARY})
+    set(BLAS_LAPACK_LIBRARIES ${EXTERNAL_PREFIX}/lib/libopenblas_sandybridgep-r0.2.8.a)
+	if( CMAKE_COMPILER_IS_GNUCC)
+		list(APPEND BLAS_LAPACK_LIBRARIES gfortran pthread)
+	endif()
+    set(BLAS_LAPACK_FOUND ${Openblas_FOUND})
+    set(BLAS_LAPACK_INCLUDE_DIRS  ${EXTERNAL_PREFIX}/include)
     set_target_properties(buildOpenblas PROPERTIES EXCLUDE_FROM_ALL 1)
 endif()
 
