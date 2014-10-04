7f3386c51fb192c8846e1296cbed0d7c0f55166d
Alessandro Rudi <alessandro.rudi@gmail.com>
2014-10-04 20:12:07 +0200

    gurls_helloworld update

--- gurls/demo/gurls_helloworld.m
+++ gurls/demo/gurls_helloworld.m
@@ -1,12 +1,12 @@
 %load the training data 
 load('data/quickanddirty_traindata'); 
 %train the classifier 
-[opt] = gurls_train(Xtr,ytr); 
+model = train(Xtr,ytr, 'nsigma', 3, 'nlambda', 5); 
 %load the test data 
 load('data/quickanddirty_testdata'); 
 %predict the labels for the test set and asses prediction accuracy 
-[yhat,acc] = gurls_test(Xte,yte,opt); 
+[yhat,acc] = test(model, Xte,yte); 
 
-disp(sprintf('\nPrediction accurcay is:'))
+disp(sprintf('\nPrediction accuracy is:'))
 disp(sprintf('\tClass %d\t',1:max(ytr)))
 disp(sprintf('\t%2.2f%%\t',acc*100))
