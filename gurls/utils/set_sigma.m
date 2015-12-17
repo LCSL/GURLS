4cd8af6cb2e7508a0aca4b182098fcb9410377ac
Jeremiah Zhe Liu <zhel90@gmail.com>
2015-12-16 22:46:07 -0500

    removed set_sigma

--- gurls/utils/set_sigma.m
+++
@@ -1,9 +0,0 @@
-function [opt] = set_sigma(opt, sigma)
-% insert sigma to the slot 'opt.paramsel.sigma'
-if ~isprop(opt,'paramsel')
-    opt.newprop('paramsel', struct());
-    % signature
-    opt.paramsel.manual_sigma = true;
-end
-opt.paramsel.sigma = sigma;
-end
